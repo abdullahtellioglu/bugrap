dfa062c0ce2f9595f3a6216b9449543f407ee403
abdullahtellioglu <tell10glu>
2022-02-21 16:55:54 +0300

    Initial commit

---
+++ src/main/java/com/vaadin/bugrap/views/helloworld/HelloWorldView.java
@@ -0,0 +1,35 @@
+package com.vaadin.bugrap.views.helloworld;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Hello World")
+@Route(value = "hello")
+@RouteAlias(value = "")
+public class HelloWorldView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloWorldView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.setIcon(new Icon(VaadinIcon.BUG));
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
